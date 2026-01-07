32042811ea4a35ef548d4ee4644a92c15c5a656f
shanewmurphy <shn.murphy@gmail.com>
2026-01-07 09:59:47 +0000

    Add logo

--- app/page.js
+++ app/page.js
@@ -43,7 +43,7 @@ function svgToPng(svgEl, scale = 4) {
 
 /* ---------- SVG PREVIEW ---------- */
 const PanelSVG = forwardRef(function PanelSVG({ widthMM, heightMM }, ref) {
-  const SCALE = 0.12;
+  const SCALE = 0.1;
   const width = Number(widthMM) * SCALE;
   const height = Number(heightMM) * SCALE;
 
@@ -245,10 +245,14 @@ export default function GlassMeasureApp() {
 
   return (
     <div className="p-8 max-w-4xl mx-auto space-y-6">
-      <h1 className="text-xl font-semibold">
-        SG Solutions Glass Measurement Recorder
-      </h1>
-
+      <div className="flex items-center justify-start">
+        <div>
+          <LogoIcon />
+        </div>
+        <div>
+          <h1 className="text-xl font-semibold pl-2">Measurement Recorder</h1>
+        </div>
+      </div>
       <input
         value={jobTitle}
         onChange={(e) => setJobTitle(e.target.value)}
@@ -281,7 +285,7 @@ export default function GlassMeasureApp() {
               <div>
                 <button
                   onClick={() => deletePanel(panel.id)}
-                  className="text-sm font-semibold rounded-sm py-1 px-3.5 bg-red-600 text-white hover:underline"
+                  className="text-sm font-semibold rounded-md py-1 px-3.5 bg-red-600 text-white hover:underline"
                 >
                   Delete
                 </button>
@@ -355,5 +359,36 @@ export default function GlassMeasureApp() {
         )}
       </div>
     </div>
+  );
+}
+
+function LogoIcon() {
+  return (
+    <svg
+      id="Layer_2"
+      data-name="Layer 2"
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 32.71 29.46"
+      className="h-14 w-14"
+    >
+      <defs>
+        <style>{`
+      .cls-1 { fill: #1d1d1b; }
+      .cls-2 { fill: #e83368; }
+    `}</style>
+      </defs>
+      <g id="Art">
+        <g>
+          <polygon
+            className="cls-2"
+            points="24.36 9.96 24.36 13.24 29.42 13.24 29.42 18.3 32.71 15.02 32.71 9.96 24.36 9.96"
+          />
+          <path
+            className="cls-1"
+            d="M13.54,27.94c0,.56-.32.89-.89.89s-.89-.33-.89-.89v-2.45h-.68v2.51c0,.9.58,1.46,1.56,1.46s1.57-.56,1.57-1.46v-2.51h-.68v2.45ZM16.07,29.39h.68v-3.33l-.68.68v2.65ZM8.85,25.49h-.68v3.9h2.27v-.57h-1.58v-3.33ZM5.21,25.42c-1.15,0-2.02.9-2.02,2.02s.87,2.02,2.02,2.02,2.02-.88,2.02-2.02-.88-2.02-2.02-2.02ZM5.21,28.84c-.77,0-1.34-.6-1.34-1.39s.56-1.39,1.34-1.39,1.33.61,1.33,1.39-.56,1.39-1.33,1.39ZM1.47,27.18c-.59-.22-.8-.4-.8-.68,0-.3.3-.48.74-.48.35,0,.72.11.95.23v-.64c-.23-.1-.61-.2-1-.2-.79,0-1.36.41-1.36,1.09,0,.63.38.94,1.09,1.19.6.21.82.38.82.69,0,.21-.17.35-.44.42-.11.03-.25.04-.39.04-.39-.01-.76-.14-1.04-.28v.67c.27.11.69.22,1.14.22.87,0,1.39-.41,1.39-1.09,0-.62-.39-.92-1.1-1.19ZM27.71,27.95l-.19-.18-2.27-2.33h-.02v3.96h.67v-2.45c.06.05.13.12.19.17l2.28,2.34h.02v-3.96h-.67v2.45ZM22.27,25.42c-1.15,0-2.02.9-2.02,2.02s.87,2.02,2.02,2.02,2.02-.88,2.02-2.02-.88-2.02-2.02-2.02ZM22.27,28.84c-.77,0-1.34-.6-1.34-1.39s.56-1.39,1.34-1.39,1.33.61,1.33,1.39-.56,1.39-1.33,1.39ZM30.83,27.18c-.59-.22-.8-.4-.8-.68,0-.3.3-.48.74-.48.35,0,.72.11.95.23v-.64c-.23-.1-.61-.2-1-.2-.79,0-1.36.41-1.36,1.09,0,.63.38.94,1.09,1.19.6.21.82.38.82.69,0,.21-.17.35-.44.42-.11.03-.25.04-.39.04-.39-.01-.76-.14-1.04-.28v.67c.27.11.69.22,1.14.22.87,0,1.39-.41,1.39-1.09,0-.62-.39-.92-1.1-1.19ZM14.94,26.07h2.95v-.57h-2.95v.57ZM18.64,29.39h.68v-3.9h-.68v3.9ZM26.81,21.59h0s2.61,0,2.61,0v-3.35h-2.61c-4.11,0-7.45-3.34-7.45-7.45h0c0-4.11,3.34-7.45,7.45-7.45h2.61V0h-2.61c-5.95,0-10.8,4.84-10.8,10.8h0c0,5.95,4.84,10.8,10.8,10.8ZM13.23,0h-2.72l-3.29,3.29h6.01V0ZM8.23,9.56c-3.37-1.17-4.63-2.19-4.63-3.77s1.39-2.32,3.61-2.5V0c-2.04.07-3.79.6-5.08,1.56C.74,2.6,0,4.09,0,5.84c0,3.57,2.36,5.09,6.16,6.39,3.53,1.19,4.72,2.16,4.72,3.83,0,1.15-.92,1.98-2.6,2.36-.58.16-1.32.24-2.24.24-2.48-.07-4.67-.93-5.83-1.48v3.41c1.87.74,4.26,1.18,6.41,1.18,4.9,0,7.83-2.19,7.83-5.85,0-3.46-2.38-4.99-6.23-6.36Z"
+          />
+        </g>
+      </g>
+    </svg>
   );
 }
